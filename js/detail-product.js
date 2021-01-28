64c2fee180ec8ca4d9cb4ed755e1bf4f9a6c8b33
ofinadir <rofynadzir1@gmail.com>
2021-01-28 23:14:35 +0700

    update detail product

---
+++ js/detail-product.js
@@ -0,0 +1,36 @@
+let thumbnails = document.querySelectorAll('.product-thumbnail');
+let activeImages = document.getElementsByClassName('active-image');
+
+for (let i = 0; i < thumbnails.length; i++) {
+
+  thumbnails[i].addEventListener('mouseover', function () {
+    if (activeImages.length > 0) {
+      activeImages[0].classList.remove('active-image')
+    }
+    this.classList.add('active-image')
+    document.querySelector('#featured').src = this.src
+  })
+}
+
+$(document).ready(function () {
+  $('#content-slider').lightSlider({
+    autoWidth: true,
+    loop: false,
+    slideEndAnimation: true,
+    slideMargin: false,
+    enableTouch: true,
+    pager: false,
+  });
+});
+
+// let buttonLeft = document.querySelector('#arrowLeft');
+// let buttonRight = document.querySelector('#arrowRight');
+
+// buttonLeft.addEventListener('click', function () {
+//   document.getElementById('slider').scrollLeft -= 180;
+// })
+
+// buttonRight.addEventListener('click', function () {
+//   document.getElementById('slider').scrollLeft += 180;
+//   console.log(buttonLeft)
+// })
