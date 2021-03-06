9f2e44b72917ff96b7cb62b432dc4cbd13c751a5
ofinadir <rofynadzir@gmail.com>
2021-03-06 09:51:31 +0700

    update all

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-$(window).scroll(function () {
-  $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
-});
+// $(window).scroll(function () {
+//   $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
+// });
 
+// $(document).ready(function(){
+//   $('#exampleModal').modal('show')
+// })
